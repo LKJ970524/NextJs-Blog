737745150ec38e955fc1dbd8f291dc97ef6a6924
LKJ970524 <dlrbwjd7096@naver.com>
2024-02-05 22:14:08 +0900

    15 포스트 데이터를 가져와서 보여주기(remark)

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { getAllPostIds, getPostData } from "../../lib/post";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+import React from "react";
+import styles from "@/styles/Home.module.css";
+import postStyle from '@/styles/Post.module.css'
 
-const Posts = () => {
+const Posts = ({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) => {
   return (
-    <div>Posts</div>
-  )
-}
+    <div className={postStyle.container}>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={styles.headingXl}>{postData.title}</h1>
+        <div className={styles.lightText}>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
+  );
+};
 
-export default Posts
+export default Posts;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  // [{params: {id: 'pre-rendering'}}, {params: {id: 'ssg-ssr'}}]
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
